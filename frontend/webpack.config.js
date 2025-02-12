17a8b40ec425f2aeb994f446e1822e5927969413
RaoulLuque <raoulsluque@gmail.com>
2025-02-12 21:27:10 +0100

    Initial setup of Frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "index.html" }
+      ]
+    })
+  ],
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        type: "webassembly/async"
+      }
+    ]
+  }
+};
